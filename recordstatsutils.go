6229e50a9ed5265f2defdfbc6bf9b0f72b9ab477
Simon Tucker <brotherlogic@gmail.com>
2020-05-19 18:09:00 -0700

    Testing the measure

--- recordstatsutils.go
+++ recordstatsutils.go
@@ -1,4 +1,21 @@
 package main
 
-func doNothing() {
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"golang.org/x/net/context"
+)
+
+var (
+	oldest = promauto.NewGauge(prometheus.GaugeOpts{
+		Name: "recordstats_oldest",
+		Help: "The oldest physical record",
+	})
+)
+
+func (s *Server) computeOldest(ctx context.Context) {
+	time.Sleep(time.Minute)
+	oldest.Set(float64(time.Now().Unix()))
 }
