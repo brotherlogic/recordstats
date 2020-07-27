213c375f08a800401fe2309284a4cefd94bac5c9
Simon Tucker <brotherlogic@gmail.com>
2020-07-27 07:50:36 -0700

    Adds some testing

--- recordstatsutils_test.go
+++ recordstatsutils_test.go
@@ -3,12 +3,19 @@ package main
 import (
 	"context"
 	"testing"
+
+	"github.com/brotherlogic/keystore/client"
+
+	pb "github.com/brotherlogic/recordstats/proto"
 )
 
 func InitTest() *Server {
 	s := Init()
 	s.SkipLog = true
 	s.Testing = true
+	s.GoServer.KSclient = *keystoreclient.GetTestClient(".test")
+	s.GoServer.KSclient.Save(context.Background(), CONFIG, &pb.Config{})
+
 	return s
 }
 
