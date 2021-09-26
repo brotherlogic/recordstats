6e7f624049fe30fcd84c7a242e9f17d34845f31b
simon <brotherlogic@gmail.com>
2021-09-25 17:10:58 -0700

    Bounce

--- recordstatsutils.go
+++ recordstatsutils.go
@@ -132,7 +132,7 @@ func (s *Server) update(ctx context.Context, id int32) error {
 			}
 		}
 		oldestLBStaged.Set(float64(time.Since(time.Unix(lax, 0)).Seconds()))
-		s.Log(fmt.Sprintf("OLDEST_LB is %v", id))
+		s.Log(fmt.Sprintf("OLDEST LB is %v", id))
 
 		laxhs := time.Now().Unix()
 		idhs := int32(-1)
@@ -143,7 +143,7 @@ func (s *Server) update(ctx context.Context, id int32) error {
 			}
 		}
 		oldestLBHigh.Set(float64(time.Since(time.Unix(lax, 0)).Seconds()))
-		s.Log(fmt.Sprintf("OLDEST_LB_HIGH is %v", idhs))
+		s.Log(fmt.Sprintf("OLDEST LB HIGH is %v", idhs))
 	}()
 
 	rec, err := s.getRecord(ctx, id)
