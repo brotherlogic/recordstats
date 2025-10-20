67f574759e4dc4942cf32b28036b05d95e5bc773
Simon Tucker <brotherlogic@gmail.com>
2025-10-20 10:38:46 -0700

    Clean logging

--- recordstatsutils.go
+++ recordstatsutils.go
@@ -538,7 +538,6 @@ func (s *Server) computeUnlistenedCDs(ctx context.Context, config *pb.Config) {
 	for id, val := range config.GetCategories() {
 		if val == rcpb.ReleaseMetadata_UNLISTENED {
 			if config.GetFiled()[id] == rcpb.ReleaseMetadata_FILE_CD && config.GetScore()[id] == 0 && config.GetWeights()[id] == 0 && !config.GetIsDirty()[id] {
-				s.CtxLog(ctx, fmt.Sprintf("FOUND_CD %v (%v)", id, config.GetScore()[id]))
 				count++
 			}
 		}
@@ -549,10 +548,8 @@ func (s *Server) computeUnlistenedCDs(ctx context.Context, config *pb.Config) {
 func (s *Server) computeUnlistened12s(ctx context.Context, config *pb.Config) {
 	count := 0
 	for id, val := range config.GetCategories() {
-		s.CtxLog(ctx, fmt.Sprintf("%v %v", id, config.GetFiled()[id]))
 		if val == rcpb.ReleaseMetadata_UNLISTENED {
 			if config.GetFiled()[id] == rcpb.ReleaseMetadata_FILE_12_INCH && config.GetScore()[id] == 0 && !config.GetIsDirty()[id] {
-				s.CtxLog(ctx, fmt.Sprintf("FOUND_12 %v (%v)", id, config.GetScore()[id]))
 				count++
 			}
 		}
@@ -565,7 +562,6 @@ func (s *Server) computeUnlistened45s(ctx context.Context, config *pb.Config) {
 	for id, val := range config.GetCategories() {
 		if val == rcpb.ReleaseMetadata_UNLISTENED || val == rcpb.ReleaseMetadata_ARRIVED {
 			if config.GetFiled()[id] == rcpb.ReleaseMetadata_FILE_7_INCH && config.GetScore()[id] == 0 && !config.GetIsDirty()[id] {
-				s.CtxLog(ctx, fmt.Sprintf("FOUND_45 %v (%v)", id, config.GetScore()[id]))
 				count++
 			}
 		}
@@ -581,7 +577,6 @@ func (s *Server) computeUnlistenedDigital(ctx context.Context, config *pb.Config
 	count := 0
 	for id, val := range config.GetCategories() {
 		if val == rcpb.ReleaseMetadata_UNLISTENED {
-			s.CtxLog(ctx, fmt.Sprintf("FOUND_DIG: %v -> %v", id, config.GetFiled()[id]))
 			if config.GetFiled()[id] == rcpb.ReleaseMetadata_FILE_DIGITAL && config.GetScore()[id] == 0 && config.GetWeights()[id] == 0 && !config.GetIsDirty()[id] {
 				count++
 			}
@@ -617,7 +612,6 @@ func (s *Server) computeOldest(ctx context.Context) (err error) {
 		}
 	}
 
-	s.CtxLog(ctx, fmt.Sprintf("Found %v - %v", r.GetRelease().GetInstanceId(), r.GetRelease().GetTitle()))
 	oldest.Set(float64(oldestTime))
 
 	return nil
